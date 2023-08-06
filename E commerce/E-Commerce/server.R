0b7d6392f0b734a4847e58d61f4fcd9abee70c26
Akhila Reddy <akhilareddyk@usf.edu>
2023-08-06 18:38:31 -0400

    commit

---
+++ E commerce/E-Commerce/server.R
@@ -0,0 +1,11 @@
+# Define server 
+
+server <- function(input, output, session) {
+  output$scatterplot <- renderPlot({
+    ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
+      geom_point()
+  })
+}
+
+
+
--- E commerce/E-Commerce/ui.R
+++ E commerce/E-Commerce/ui.R
@@ -1,88 +1,48 @@
+# Load packages 
 
 library(shiny)
-library(shinydashboard)
+library(ggplot2)
 
-# Product data (you can replace this with your product data)
-products <- data.frame(
-  ProductID = c(1, 2, 3, 4),
-  ProductName = c("Product A", "Product B", "Product C", "Product D"),
-  Price = c(10, 15, 20, 25)
-)
+# Get the data
 
-ui <- dashboardPage(
-  dashboardHeader(
-    title = "Simple E-commerce Website",
-    tags$li(
-      class = "dropdown",
-      tags$a(
-        href = "#",
-        class = "dropdown-toggle",
-        `data-toggle` = "dropdown",
-        icon("bars"), # Font Awesome menu icon
-        "Menu"
+file <- "https://github.com/rstudio-education/shiny-course/raw/main/movies.RData"
+destfile <- "movies.RData"
+
+download.file(file, destfile)
+
+# Load data 
+
+load("movies.RData")
+
+# Define UI 
+
+ui <- fluidPage(
+  
+  sidebarLayout(
+    
+    # Inputs: Select variables to plot
+    sidebarPanel(
+      
+      # Select variable for y-axis
+      selectInput(
+        inputId = "y",
+        label = "Y-axis:",
+        choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+        selected = "audience_score"
       ),
-      tags$ul(
-        class = "dropdown-menu",
-        tags$li(tags$a(href = "#", icon("home"), "Home")),
-        tags$li(tags$a(href = "#", icon("info-circle"), "About")),
-        tags$li(tags$a(href = "#", icon("shopping-cart"), "Cart"))
+      # Select variable for x-axis
+      selectInput(
+        inputId = "x",
+        label = "X-axis:",
+        choices = c("imdb_rating", "imdb_num_votes", "critics_score", "audience_score", "runtime"),
+        selected = "critics_score"
       )
-    )
-  ),
-  dashboardSidebar(),
-  dashboardBody(
-    fluidRow(
-      box(
-        width = 4,
-        title = "Product Listing",
-        tableOutput("product_table")
-      ),
-      box(
-        width = 4,
-        title = "Shopping Cart",
-        tableOutput("cart_table"),
-        actionButton("checkout_button", "Checkout")
-      )
+    ),
+    
+    # Output: Show scatterplot
+    mainPanel(
+      plotOutput(outputId = "scatterplot")
     )
   )
 )
-
-server <- function(input, output, session) {
-  # Initialize the shopping cart as an empty data frame
-  cart <- reactiveVal(data.frame(ProductID = integer(), ProductName = character(), Price = numeric()))
-  
-  # Render the product listing table
-  output$product_table <- renderTable({
-    products
-  })
-  
-  # Update the shopping cart when a product is added
-  observeEvent(input$product_table_rows_selected, {
-    selected_row <- input$product_table_rows_selected
-    if (length(selected_row) > 0) {
-      product <- products[selected_row, ]
-      current_cart <- cart()
-      updated_cart <- rbind(current_cart, product)
-      cart(updated_cart)
-    }
-  })
-  
-  # Render the shopping cart table
-  output$cart_table <- renderTable({
-    cart()
-  })
-  
-  # Perform checkout when the checkout button is clicked
-  observeEvent(input$checkout_button, {
-    # Implement your checkout logic here (e.g., payment processing, order confirmation)
-    # For this example, we'll just reset the cart to empty after checkout
-    cart(data.frame(ProductID = integer(), ProductName = character(), Price = numeric()))
-    showModal(modalDialog(
-      title = "Checkout Successful",
-      "Thank you for your purchase!",
-      easyClose = TRUE
-    ))
-  })
-}
-
 
