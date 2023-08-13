d7f638adc79fc2660dd5a4041b07ad39d3e85249
Akhila Reddy <akhilareddyk@usf.edu>
2023-08-13 18:27:18 -0400

    initial commit

--- Validation app/Validation_app/server.R
+++ Validation app/Validation_app/server.R
@@ -1,13 +1,23 @@
-server <- function(input, output) {
-  
-  data <- reactive({ get(input$data, 'package:datasets') })
-  
-  output$plot <- renderPlot({
-    hist(data()[, 1], col = 'forestgreen', border = 'white')
+library(shiny)
+library(readr)
+
+shinyServer(function(input, output) {
+  observeEvent(input$file, {
+    file <- input$file$datapath
+    file_extension <- tools::file_ext(file)
+    
+    if (file_extension != "csv") {
+      output$alert <- renderPrint({
+        "Alert: Only CSV files are allowed."
+      })
+      return()
+    }
+    
+    data <- read_csv(file)
+    num_rows <- nrow(data)
+    
+    output$result <- renderPrint({
+      paste("Number of rows in the uploaded CSV:", num_rows)
+    })
   })
-  
-  output$table <- renderTable({
-    head(data())
-  })
-  
-}
+})
